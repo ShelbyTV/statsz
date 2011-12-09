583713a81f5e430c20cf55763ce2d865916141d0
Henry Sztul <henry@sztul.com>
2011-12-08 19:38:56 -0500

    cleaning up js code a little

--- js/libs/graphs_google.js
+++ js/libs/graphs_google.js
@@ -2,9 +2,6 @@ $(document).ready(function(){
 	google.setOnLoadCallback(drawCharts);
   function drawCharts() {
     /* activity_chart  */
-  	var activity_data = new google.visualization.DataTable();
-  	activity_data.addColumn('string', 'Date');
-  	activity_data.addColumn('number', 'DAU');
     var width = $("#activity_chart").width();
   	var activity_options = {
   		width: width, height: 100,
@@ -13,21 +10,32 @@ $(document).ready(function(){
   		chartArea: {width: '90%'}
   	};
   	
+  	plotActivityChart = function(){
+  	  var activity_data = new google.visualization.DataTable();
+    	activity_data.addColumn('string', 'Date');
+    	activity_data.addColumn('number', 'DAU');
+
+      // get data from graphite, iterate, pass into data via addRow
+      getData('stats.activity.daily', '10min', '-7d', false, 'addActivityData');
+      addActivityData = function(d){
+        d.datapoints.forEach(function(e){
+          activity_data.addRow([Date(e[1]),e[0]]);
+        });
+      	activity_chart.draw(activity_data, activity_options);
+      };
+  	};
+  	
   	var activity_chart = new google.visualization.LineChart(document.getElementById('activity_chart'));
 
-    // get data from graphite, iterate, pass into data via addRow
-    getData('stats.activity.daily', '10min', '-7d', false, 'addActivityData');
-    addActivityData = function(d){
-      d.datapoints.forEach(function(e){
-        activity_data.addRow([Date(e[1]),e[0]]); // dividing by 6 because of interval of 60 min above (data passed to graphite every 10min)
-      });
-    	activity_chart.draw(activity_data, activity_options);
-    };
+    // plot it once
+  	plotActivityChart(); 
+  	// and plot it once a minute thereafter
+    setInterval(function(){
+    	plotActivityChart();
+    },60000);
     
     /* retention_chart  */
-  	var retention_data = new google.visualization.DataTable();
-  	//data.addColumn('string', 'Date');
-  	retention_data.addColumn('number', 'DAU');
+  	var retention_chart = new google.visualization.LineChart(document.getElementById('retention_chart'));
   	var retention_options = {
   		width: '100%', height: 100,
   		title: "retention: past 7 days",
@@ -35,19 +43,23 @@ $(document).ready(function(){
   		chartArea: {width: '90%'}
   	};
   	
-  	var retention_chart = new google.visualization.LineChart(document.getElementById('retention_chart'));
-
-    // get data from graphite, iterate, pass into data via addRow
+  	// get data from graphite, iterate, pass into data via addRow
     /* TODO: CHANGE THIS CALL TO GET CORRECT DATA  */
-    getData('stats.activity.daily', '10min', '-7d', false, 'addRetentionData');
-    addRetentionData = function(d){
-      d.datapoints.forEach(function(e){
-        retention_data.addRow([e[0]]); // dividing by 6 because of interval of 60 min above (data passed to graphite every 10min)
-      });
-    	retention_chart.draw(activity_data, retention_options);
-    };
+    plotRetentionChart = function(){
+  	  var retention_data = new google.visualization.DataTable();
+    	retention_data.addColumn('string', 'Date');
+    	retention_data.addColumn('number', 'Retention');
+
+      // get data from graphite, iterate, pass into data via addRow
+      getData('stats.activity.daily', '10min', '-7d', false, 'addRetentionData');
+      addRetentionData = function(d){
+        d.datapoints.forEach(function(e){
+          retention_data.addRow([Date(e[1]),e[0]]); 
+        });
+      	retention_chart.draw(retention_data, activity_options);
+      };
+  	};
     
-    
-    
+    plotRetentionChart();
   }  
 });
